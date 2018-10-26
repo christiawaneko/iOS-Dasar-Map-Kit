8f7b1865450eeeda8690ab8c0634e6fa74b57caa
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-26 12:09:39 +0700

    Simlpe Map Kit

--- Maps Kit/ViewController.swift
+++ Maps Kit/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var peta: MKMapView!
+    var gps = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // menggunakan tipe map yang standard
+        peta.mapType = .standard
+        
+        
+        let coordinat = CLLocationCoordinate2D( latitude: -6.3403091, longitude: 106.7562961)
+        let span = MKCoordinateSpan(latitudeDelta: 0.10, longitudeDelta: 0.10)
+        peta.region = MKCoordinateRegion(center: coordinat, span: span)
+
+        let nin = MKPointAnnotation()
+        nin.title = "Seamolec"
+        nin.subtitle = "Instansi Pendidikan"
+        nin.coordinate = coordinat
+        peta.addAnnotation(nin)
+        
+        
+        
+        gps.delegate = self
+        gps.startUpdatingLocation()
+        gps.requestLocation()
+        gps.requestWhenInUseAuthorization()
+        
+        
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations != nil {
+            print(locations)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
 
